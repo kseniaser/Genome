948b3c23e957aa8c6adfe1bbd96ed1999bc12eff
Ashkroft <polina.university@yandex.ru>
2018-02-19 00:48:44 +0300

    trying testing

---
+++ src/test/java/Polina/Ksusha/ApplicationTest.java
@@ -0,0 +1,60 @@
+package Polina.Ksusha;
+
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static org.testng.Assert.*;
+
+public class ApplicationTest {
+    //    public MyGraph expectedGraph() {
+//        MyGraph graph = new MyGraph();
+//        graph.addNode("AA");
+//        graph.addNode("AB");
+//        graph.addNode("BB");
+//        graph.addNode("BA");
+//
+//        graph.addEdge("AAA");
+//        graph.addEdge("AAB");
+//        graph.addEdge("ABB");
+//        graph.addEdge("BBB");
+//        graph.addEdge("BBA");
+//        return graph;
+//    }
+
+//        private MyGraph testGraph = new MyGraph();
+//        @BeforeTest
+//        public void uploadGraphs (String args[]){
+//            try(Scanner in = new Scanner(new File(args[0]));) {
+//                while (in.hasNextLine()) {
+//                    String str = in.nextLine();
+//                        graph.addNode(a);
+//                        graph.addNode(b);
+//                        graph.addEdge(str);
+//
+//                    }
+//                }
+//            } catch (FileNotFoundException ex) {
+//                ex.printStackTrace();
+//            }
+//
+//        }
+//
+//
+//        @DataPovider
+//        public Object[][] graphs() {
+//        }
+
+
+        @Test
+        public void reader1() throws IOException {
+                //     MyGraph expected = new ApplicationTest().expectedGraph();
+                //   MyGraph actual = new MyReader().readGraph(args[0]) ;
+                //    assertTrue(actual.equal(expected));
+
+            }
+
+}
