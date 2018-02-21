21bd0ac11575c6ab3ccbc6d54454ed7e0fc6583e
Ashkroft <polina.university@yandex.ru>
2018-02-21 21:40:49 +0300

    deserialize rebased

--- src/main/java/Polina/Ksusha/MyGraph.java
+++ src/main/java/Polina/Ksusha/MyGraph.java
@@ -1,5 +1,7 @@
 package Polina.Ksusha;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Array;
 import java.util.*;
 
@@ -24,6 +26,26 @@ public class MyGraph {
     public boolean equal(MyGraph gr2) {
         if ((this.edges.equals(gr2.edges)) && (this.nodes.equals(gr2.nodes))) return true;
         else return false;
+    }
+
+    public MyGraph deserialize(String filename){
+        MyGraph expG = new MyGraph();
+        int cnt = 0;
+        try (Scanner in = new Scanner(new File(filename))) {
+            String ns = in.nextLine();
+            int n = Integer.parseInt(ns);
+            String ms = in.nextLine();
+            int m = Integer.parseInt(ms);
+            while (in.hasNextLine()) {
+                String str = in.nextLine();
+                if (cnt < n) expG.addEdge(str);
+                else expG.addNode(str);
+                cnt++;
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return expG;
     }
 
 }
