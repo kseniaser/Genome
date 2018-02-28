149fec6f0d148bd01df2aabb1861d09ffdac8751
Ashkroft <polina.university@yandex.ru>
2018-02-28 18:32:04 +0300

    FastqReader added

--- src/main/java/Polina/Ksusha/MyReader.java
+++ src/main/java/Polina/Ksusha/MyReader.java
@@ -1,15 +1,20 @@
 package Polina.Ksusha;
-
+import htsjdk.samtools.fastq.FastqReader;
 import java.util.*;
 import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class MyReader {
 
     public static MyGraph readGraph(String inFile)  {
         MyGraph graph = new MyGraph();
-        try(Scanner in = new Scanner(new File(inFile));) {
-            while (in.hasNextLine()) {
-                String str = in.nextLine();
+        try(FastqReader in = new FastqReader(new File(inFile));) {
+            while (in.hasNext()) {
+                String str = in.next().getReadString();
+//                System.out.println(str);
+//                System.out.println();
                 if (!str.equals("")) {
                     String a = str.substring(0, str.length() - 1);
                     String b = str.substring(1);
@@ -19,8 +24,6 @@ public class MyReader {
 
                 }
             }
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
         }
         return graph;
     }
